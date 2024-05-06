d14f3e5828b51b70268ef442190ec689c25b79ce
Viktar <1105714@gmail.com>
2024-05-06 17:29:25 +0400

    disable audio.h

--- examples/addon.node.stream/audio.h
+++ examples/addon.node.stream/audio.h
@@ -1,17 +1,17 @@
-#pragma once
+// #pragma once
 
-#include <string>
-#include <vector>
-#include "common-sdl.h"
-#include "common.h"
+// #include <string>
+// #include <vector>
+// #include "common-sdl.h"
+// #include "common.h"
 
 
 
-// class audio_async;
-// class wav_writer;
+// // class audio_async;
+// // class wav_writer;
 
-bool init_audio(audio_async& audio, int capture_id, int sample_rate);
-void create_wav_writer(wav_writer& wavWriter, const std::string& filename, int sample_rate);
-std::string get_current_datetime();
-bool process_audio_non_vad(audio_async& audio, std::vector<float>& pcmf32_new, int n_samples_step, int step_ms);
-bool process_audio_vad(audio_async& audio, std::vector<float>& pcmf32_new, std::vector<float>& pcmf32, int sample_rate, int length_ms, float vad_thold, float freq_thold);
+// bool init_audio(audio_async& audio, int capture_id, int sample_rate);
+// void create_wav_writer(wav_writer& wavWriter, const std::string& filename, int sample_rate);
+// std::string get_current_datetime();
+// bool process_audio_non_vad(audio_async& audio, std::vector<float>& pcmf32_new, int n_samples_step, int step_ms);
+// bool process_audio_vad(audio_async& audio, std::vector<float>& pcmf32_new, std::vector<float>& pcmf32, int sample_rate, int length_ms, float vad_thold, float freq_thold);
